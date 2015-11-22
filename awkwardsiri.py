24d89c9b26e9920ae071f7d158dafcb1aac3840f
RongYu98 <Stuy.rong.yu@gmail.com>
2015-11-22 17:18:46 -0500

    siri will not remove all the words in 20k.txt and CommonWords from the dictionary

--- awkwardsiri.py
+++ awkwardsiri.py
@@ -6,15 +6,38 @@ l=[]
 for result in r:
     l.append(result)
 
-#print l[0]
-
 u = urllib2.urlopen(l[0])
 page = u.read()
 #print page
 soup = bs4.BeautifulSoup(page)
 raw = soup.get_text()
 #print raw
+
 text = re.findall("[A-Z][a-z]+ [A-Z][a-z]+",raw)
+
+file = open("20k.txt", "r")
+words = file.read()
+file.close()
+content = words.split("\n") #content is now a list of strings of words
+for word in content:
+    word = word.lower()
+for word in text:
+    twoWords = word.split(" ")
+    if ( (twoWords[0].lower() in content) or (twoWords[1].lower() in content) ):
+        text.remove(word)
+file = open("CommonWords.csv", "r")
+words = file.read()
+file.close()
+content = words.split("\n") #content is now a list of strings of words
+for word in content:
+    word = word.lower()
+for word in text:
+    twoWords = word.split(" ")
+    if ( (twoWords[0].lower() in content) or (twoWords[1].lower() in content) ):
+        text.remove(word)
+
+
+
 dict = {}
 for name in text:
     if name in dict.keys():
@@ -22,6 +45,6 @@ for name in text:
     else:
         dict[name]=1;
 
-for key in sorted(dict):
-    print "%s: %s" % (key, dict[key])
-#print dict
+#for key in sorted(dict):
+    #print "%s: %s" % (key, dict[key])
+print dict
