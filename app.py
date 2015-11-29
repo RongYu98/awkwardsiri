7d504010db9713cb79704ae28104b54a01697912
RongYu98 <Stuy.rong.yu@gmail.com>
2015-11-29 13:02:57 -0500

    directly accessing app.py now works, added a home template for both answers and question, added protection from bad websites, things in awksiri are now functions

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+import awkwardsiri, urllib2, google, bs4, re
+
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def main():
+    if request.method == "GET":
+        return render_template("home.html")
+    query = request.form["question"]
+    answer = awkwardsiri.finder(query)
+    print "-----------------------------------------------"
+    print answer
+    print "-----------------------------------------------"
+    return render_template("home.html", response = answer, question = query)
+
+
+if (__name__ == "__main__"):
+        app.debug = True
+        app.secret_key = "secret"
+        app.run(host='0.0.0.0', port=8000)
--- awkwardsiri.py
+++ awkwardsiri.py
@@ -1,70 +1,94 @@
 import urllib2, google, bs4, re
 
-q="who played spiderman"
-r = google.search(q,num=10,start=0,stop=10)
-l=[]
-for result in r:
-    l.append(result)
-dict = {}
+def finder(str):
+    #q="who played spiderman"
+    q = str
+    r = google.search(q,num=10,start=0,stop=10)
+    l=[]
+    for result in r:
+	print result
+        l.append(result)
+    dict = {}
     
-for i in range(0,11):
-    u = urllib2.urlopen(l[i])
-    page = u.read()
-    #print page
-    soup = bs4.BeautifulSoup(page)
-    raw = soup.get_text()
-    #print raw
+    for i in range(0,11):
+        try:
+            print "------------"
+            print l[i]
+            u = urllib2.urlopen(l[i])
+            page = u.read()
+            #print page
+            soup = bs4.BeautifulSoup(page)
+            raw = soup.get_text()
+            #print raw
     
-    text = re.findall("[A-Z][a-z]+ [A-Z][a-z]+",raw)
+            text = re.findall("[A-Z][a-z]+ [A-Z][a-z]+",raw)
     
-    file = open("20k.txt", "r")
-    words = file.read()
-    file.close()
-    content = words.split("\n") #content is now a list of strings of words
-    for word in content:
-        word = word.lower()
-    for word in text:
-        twoWords = word.split(" ")
-        #if ( (twoWords[0].lower() in content) or (twoWords[1].lower() in content) ):
-            #text.remove(word)
-    file = open("CommonWords.csv", "r")
-    words = file.read()
-    file.close()
-    content = words.split("\n") #content is now a list of strings of words
-    for word in content:
-        word = word.lower()
-    for word in text:
-        twoWords = word.split(" ")
-        if ( (twoWords[0].lower() in content) or (twoWords[1].lower() in content) ):
-            text.remove(word)
+            file = open("20k.txt", "r")
+            words = file.read()
+            file.close()
+            content = words.split("\n") #content is now a list of strings of words
+            for word in content:
+                word = word.lower()
+            for word in text:
+                twoWords = word.split(" ")
+                #if ( (twoWords[0].lower() in content) or (twoWords[1].lower() in content) ):
+                    #text.remove(word)
+            file = open("CommonWords.csv", "r")
+            words = file.read()
+            file.close()
+            content = words.split("\n") #content is now a list of strings of words
+            for word in content:
+                word = word.lower()
+            for word in text:
+                twoWords = word.split(" ")
+                if ( (twoWords[0].lower() in content) or (twoWords[1].lower() in content) ):
+                    text.remove(word)
 
-    file = open("names.csv", "r")
-    names = file.read()
-    file.close()
-    content = names.split(",")
-    for word in content:
-        if word.lower in text:
-            print word
-            text.remove(word)
+            file = open("names.csv", "r")
+            names = file.read()
+            file.close()
+            content = names.split(",")
+            for word in content:
+                if word.lower in text:
+                    print word
+                    text.remove(word)
             
-    for name in text:
-        if name in dict.keys():
-            dict[name]+=1;
-        else:
-            dict[name]=1;
+            for name in text:
+                if name in dict.keys():
+                    dict[name]+=1;
+                else:        
+                    dict[name]=1;
 
-for key in sorted(dict):
-    if dict[key] < 21:
-        dict.pop(key)
+        except urllib2.HTTPError, e:
+            print(e.code)
+        except urllib2.URLError, e:
+            print(e.args)
 
-for key in sorted(dict):
-    print "%s: %s" % (key, dict[key])
+    return findmax(dict)
 
-max = 0;
-maxKey = ""
-for key in dict:
-    if dict[key] > max:
-        maxKey = key
-	max = dict[key]
 
-print "The answer is most likely: %s" % (maxKey)
+
+
+def findmax(dict):
+    for key in sorted(dict):
+        if dict[key] < 21:
+            dict.pop(key)
+
+    for key in sorted(dict):
+        print "%s: %s" % (key, dict[key])
+
+    max = 0;
+    maxKey = ""
+    for key in dict:
+        if dict[key] > max:
+            maxKey = key
+	    max = dict[key]
+
+    return "The answer is most likely: %s" % (maxKey)
+
+
+#finder("who played spiderman")
+#Sample Questions: 
+#Where is Stuyvesant
+#Who is  Mike Zamansky?
+#Who is the strongest man alive?
